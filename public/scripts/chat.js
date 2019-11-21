68cabcbe860c29057709ebd35703b1441c0b9f9d
Bruno Mendez <bruno.m_@hotmail.com>
2019-11-21 02:49:06 -0600

    scroll down when receiving a message

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,28 +1,29 @@
-$('form a').click(function(event){
+$('form a').click(function (event) {
     event.preventDefault();
-    $(this).closest('form').attr('action', this.href ).attr('method', 'post').submit();
+    $(this).closest('form').attr('action', this.href).attr('method', 'post').submit();
 });
 let cont = 0;
-var interval = setInterval(function() {
-    fetch('/get-messages/' + id)    
-    .then(response => {
-        if(response.ok) {
-          return response.json();
-        }
-        throw new Error (response.statusText);
-      })
-      .then(responseJSON => {
-          for (let i = cont; i < responseJSON.messages.length; i++) {
-              let message = responseJSON.messages[i];
-              cont++;
-              insertChat(message.isListener, message.message, message.name);
-          }
-      })
-      .catch(error => {
-        insertChat(!isListener, "User left", "Admin");
-        setTimeout(function() { window.location.reload(false);}, 3000);
-        clearInterval(interval);
-      });
+var interval = setInterval(function () {
+    fetch('/get-messages/' + id)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJSON => {
+            for (let i = cont; i < responseJSON.messages.length; i++) {
+                let message = responseJSON.messages[i];
+                cont++;
+                insertChat(message.isListener, message.message, message.name);
+                setTimeout(function() {$("#message-list").scrollTop($("#message-list")[0].scrollHeight);}, 50);
+            }
+        })
+        .catch(error => {
+            insertChat(!isListener, "User left", "Admin");
+            setTimeout(function () { window.location.reload(false); }, 3000);
+            clearInterval(interval);
+        });
 }, 1000);
 
 function formatAMPM(date) {
@@ -31,54 +32,54 @@ function formatAMPM(date) {
     var ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
-}            
+}
 
 //-- No use time. It is a javaScript effect.
-function insertChat(isListener, text, senderName, time = 0){
+function insertChat(isListener, text, senderName, time = 0) {
     var control = "";
     var date = formatAMPM(new Date());
-    
-    if (!isListener){
+
+    if (!isListener) {
         control = '<li style="width:100%">' +
-                        '<div class="msj macro">' +
-                            '<div class="text text-l">' +
-                                '<p class="name-in-msg-listener">' + senderName + '</p>' +
-                                '<p>'+ text +'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '</div>' +
-                    '</li>';                    
+            '<div class="msj macro">' +
+            '<div class="text text-l">' +
+            '<p class="name-in-msg-listener">' + senderName + '</p>' +
+            '<p>' + text + '</p>' +
+            '<p><small>' + date + '</small></p>' +
+            '</div>' +
+            '</div>' +
+            '</li>';
     } else {
         control = '<li style="width:100%;">' +
-                        '<div class="msj-rta macro">' +
-                            '<div class="text text-r">' +
-                                '<p class="name-in-msg-anon">' + senderName + '</p>' +
-                                '<p>'+text+'</p>' +
-                                '<p><small>'+date+'</small></p>' +
-                            '</div>' +
-                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"></div>' +                                
-                  '</li>';
+            '<div class="msj-rta macro">' +
+            '<div class="text text-r">' +
+            '<p class="name-in-msg-anon">' + senderName + '</p>' +
+            '<p>' + text + '</p>' +
+            '<p><small>' + date + '</small></p>' +
+            '</div>' +
+            '<div class="avatar" style="padding:0px 0px 0px 10px !important"></div>' +
+            '</li>';
     }
     setTimeout(
-        function(){                        
+        function () {
             $("#message-list").append(control);
 
         }, time);
-    
+
 }
 
-function resetChat(){
+function resetChat() {
     $("#message-list").empty();
 }
 
-$(".mytext").on("keyup", function(e){
-    if (e.which == 13){
+$(".mytext").on("keyup", function (e) {
+    if (e.which == 13) {
         console.log(isListener);
         var text = $(this).val();
-        if (text !== ""){
+        if (text !== "") {
 
             let message = {
                 name: name,
@@ -103,7 +104,7 @@ $(".mytext").on("keyup", function(e){
 resetChat();
 
 //-- Print Messages
-insertChat("me", "Hello Tom...", 0);  
+insertChat("me", "Hello Tom...", 0);
 insertChat("you", "Hi, Pablo", 1500);
 insertChat("me", "What would you like to talk about today?", 3500);
 insertChat("you", "Tell me a joke",7000);
